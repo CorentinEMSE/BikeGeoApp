3eb6a332431c2bc5f5c9a723fe0c98a27ecc5899
Corentin <corentin.marchand@etu.emse.fr>
2016-10-17 13:21:29 +0200

    Ajout de l'interface de base : Navigation activity crée 3 fragments grâce au PagerAdapter. Ces 3 fragments correspondent respectivement aux trois boutons en haut de l'interface "Liste, Carte, Info" et ces 3 types de fragment sont déclarés dans les fichiers ListFragment,MapFragment et InfoFragment.
    On peut passer d'un fragment à l'autre en cliquant sur les boutons du haut ou en faisant glisser l'écran.
    Testé et validé le 17/10

---
+++ app/src/main/java/corentinulysse/bikegeoapp/NavigationActivity.java
@@ -0,0 +1,75 @@
+package corentinulysse.bikegeoapp;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class NavigationActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        // Get the ViewPager and set it's PagerAdapter so that it can display items
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(),
+//                NagivationActivity.this));
+//
+//        // Give the TabLayout the ViewPager
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+//        tabLayout.setupWithViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/corentinulysse/bikegeoapp/PagerAdapter.java
@@ -0,0 +1,60 @@
+package corentinulysse.bikegeoapp;
+
+
+/**
+ * Created by Corentin on 17/10/2016.
+ */
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+    final int PAGE_COUNT=3;
+    private String tabTitles[] = new String[] { "Liste", "Carte", "Info" };
+    private Context context;
+
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs, Context context) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+        this.context=context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                ListFragment tab1 = new ListFragment();
+                return tab1;
+            case 1:
+                MapFragment tab2 = new MapFragment();
+                return tab2;
+            case 2:
+                InfoFragment tab3 = new InfoFragment();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}

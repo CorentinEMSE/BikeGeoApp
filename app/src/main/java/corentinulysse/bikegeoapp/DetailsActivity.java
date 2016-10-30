6faa662d001be4f069dff4344d6183ac8679d928
Corentin <corentin.marchand@etu.emse.fr>
2016-10-30 20:11:43 +0100

    Ajout des notifications dans la map en cas de perte GPS/Network

--- app/src/main/java/corentinulysse/bikegeoapp/DetailsActivity.java
+++ app/src/main/java/corentinulysse/bikegeoapp/DetailsActivity.java
@@ -2,6 +2,9 @@ package corentinulysse.bikegeoapp;
 
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +15,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,7 +26,17 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback, android.location.LocationListener {
+
+
+    private LocationManager lm;
+    private LocationListener locationListener;
+
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float accuracy;
+
 
     private StationsVelib mStation;
     private Menu mMenu;
@@ -176,6 +191,94 @@ public class DetailsActivity extends AppCompatActivity implements OnMapReadyCall
         mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
         // mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(mTargetMarker));
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        super.onResume();
+        lm = (LocationManager)getSystemService(LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+//                return;
+            }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, (android.location.LocationListener) this);
+        if(lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, (android.location.LocationListener) this);
+
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+//            return;
+        }
+        lm.removeUpdates((android.location.LocationListener) this);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider){
+        String msg = String.format(getResources().getString(R.string.provider_enabled), provider);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    public void onProviderDisabled(String provider){
+        String msg = String.format(getResources().getString(R.string.provider_disabled), provider);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status,Bundle extras ){
+        String newStatus = "";
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                newStatus = "OUT_OF_SERVICE";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                newStatus = "TEMPORARILY_UNAVAILABLE";
+                break;
+            case LocationProvider.AVAILABLE:
+                newStatus = "AVAILABLE";
+                break;
+        }
+
+        String msg = String.format(getResources().getString(R.string.provider_new_status), provider,newStatus);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        altitude = location.getAltitude();
+        accuracy = location.getAccuracy();
+
+        String msg = String.format(
+                getResources().getString(R.string.new_location), String.valueOf(latitude),
+                String.valueOf(longitude),String.valueOf(altitude), String.valueOf(accuracy));
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 
     }
 }
